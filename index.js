ecb495c6dad4891bd81529bcc41ba8b6be12c2fb
lovleenkaur <lovleenk99@gmail.com>
2023-06-14 10:13:06 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement("h1");
+//document.appendChild(newHeader);
+
+//newHeader.setAttribute("id", "victory");
+newHeader.id = 'victory'
+
+//const diffHeader = document.getElementById("victory");
+newHeader.textContent = "Lovleen Kaur is the champion"
+
